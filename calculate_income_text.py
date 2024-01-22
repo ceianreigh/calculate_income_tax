bef5003813cf9918782164545146760dea063557
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 21:15:51 +0800

    Add the instruction and pseudocode

---
+++ calculate_income_text.py
@@ -0,0 +1,14 @@
+# Calculate income tax for the given income by adhering to the rules
+# First 10000 is 0 tax, the next 10000 is 10% tax, the remaining is 20% tax
+
+# pseudocode
+
+# ask user for taxable income
+
+# if income is less than 10000
+
+# else if income is less than 20000
+
+# else
+
+# print the tax
