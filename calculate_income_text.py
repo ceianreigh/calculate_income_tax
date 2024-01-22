363e9003745701fc6120f53262c5148312724381
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 21:24:20 +0800

    Write tax calculation based on income using if-else statement

--- calculate_income_text.py
+++ calculate_income_text.py
@@ -7,9 +7,15 @@
 taxable_income = int(input("Enter your taxable income: "))
 
 # if income is less than 10000
+if taxable_income < 10000:
+    tax = 0
 
 # else if income is less than 20000
+elif taxable_income < 20000:
+    tax = (taxable_income - 10000) * 0.1
 
 # else
+else:
+    tax = 10000 * 0 + 10000 * 0.1 + (taxable_income - 20000) * 0.2
 
 # print the tax
