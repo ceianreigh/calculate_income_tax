3b49a9809d53f5e13671ef88df928b82558cc59d
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 21:08:20 +0800

    Write the logic that calculates the taxable income

--- calculate_income_tax.py
+++ calculate_income_tax.py
@@ -8,10 +8,13 @@ taxable_income = int(input("Enter your income: "))
 
 # calculate the tax based on the taxable income
 # if the taxable income is less than 10000
-#     tax = 0
+if taxable_income < 10000:
+    tax = 0
 # else if the taxable income is less than 20000
-#     tax = 10000 * 0 + (taxable_income - 10000) * 0.1
+elif taxable_income < 20000:
+    tax = 0 + (taxable_income - 10000) * 0.1
 # else
-#     tax = 10000 * 0 + 10000 * 0.1 + (taxable_income - 20000) * 0.2
+else:
+    tax = 0 + 10000 * 0.1 + (taxable_income - 20000) * 0.2
 
 # print the tax
